970ce276cd478c3c462a07a6a96470161efb6e47
raymondmg <raymondmgwx@gmail.com>
2021-10-05 23:10:12 +0900

    add 2d shape data

--- scripts/alphashape/vislization.py
+++ scripts/alphashape/vislization.py
@@ -1,7 +1,7 @@
 '''
 Author: Xu.WANG
 Date: 2021-03-24 23:23:19
-LastEditTime: 2021-09-14 19:18:35
+LastEditTime: 2021-10-05 22:56:06
 LastEditors: Xu.WANG
 Description: 
 FilePath: \Kiri2D\scripts\alphashape\vislization.py
@@ -14,7 +14,7 @@ import alphashape
 
 points = []
 
-path = "D:/project/Kiri/export/xy/cow.xy"
+path = "D:/project/Kiri/export/xy/alligator.xy"
 with open(path) as f:
     l_strip = [s.strip() for s in f.readlines()]
     for l in l_strip:
@@ -22,9 +22,10 @@ with open(path) as f:
             p2 = l.split(" ")
             points.append([float(p2[0]), float(p2[2])])
 
-alpha_shape = alphashape.alphashape(points, 20.)
+alpha_shape = alphashape.alphashape(points, 200.)
 
 fig, ax = plt.subplots()
 # ax.scatter(*zip(*points))
 ax.add_patch(PolygonPatch(alpha_shape, alpha=0.2))
 plt.show()
+
