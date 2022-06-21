67f206ae237f609dc477bc1e613d1bdfcff521d3
Xu.WANG <raymondmgwx@gmail.com>
2022-06-21 11:47:17 +0900

    update

--- demos/interior_point_method/include/ipm.h
+++ demos/interior_point_method/include/ipm.h
@@ -2,7 +2,7 @@
  * @Author: Xu.WANG raymondmgwx@gmail.com
  * @Date: 2022-06-15 23:16:19
  * @LastEditors: Xu.WANG raymondmgwx@gmail.com
- * @LastEditTime: 2022-06-17 10:50:03
+ * @LastEditTime: 2022-06-21 09:06:38
  * @FilePath: \Kiri2D\demos\interior_point_method\include\ipm.h
  * @Description:
  * @Copyright (c) 2022 by Xu.WANG raymondmgwx@gmail.com, All Rights Reserved.
@@ -13,13 +13,21 @@
 #pragma once
 
 #include <sstream>
+
 #include <Eigen/LU>
+#include <Eigen/QR>
+
 #include <autodiff/forward/dual.hpp>
 #include <autodiff/forward/dual/eigen.hpp>
+#include <autodiff/forward/real.hpp>
+#include <autodiff/forward/real/eigen.hpp>
+
+using namespace autodiff;
+using std::ostringstream;
 
 using Eigen::EigenBase;
-using std::ostringstream;
-using namespace autodiff;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
 
 // dual2nd targetFunc(const VectorXdual2nd &x)
 // {
@@ -27,9 +35,29 @@ using namespace autodiff;
 // }
 
 // minimize f(x, y) = 100*(y - x**2)**2 + (1 - x)**2
-dual2nd targetFunc(const VectorXdual2nd &x)
+// dual2nd targetFunc(const VectorXdual2nd &x)
+// {
+//     return 100 * (x[1] - x[0] * x[0]) * (x[1] - x[0] * x[0]) + (1 - x[0]) * (1 - x[0]);
+// }
+
+real Constrainst0(const ArrayXreal &x)
 {
-    return 100 * (x[1] - x[0] * x[0]) * (x[1] - x[0] * x[0]) + (1 - x[0]) * (1 - x[0]);
+    return x[0] + 2 * x[1] - 10;
+}
+
+VectorXreal Constrainst1(const VectorXreal &x)
+{
+    return x;
+}
+
+dual2nd ConstrainstFunc(const VectorXdual2nd &x, const MatrixXd &lambda)
+{
+    return (x[0] + 2 * x[1] - 10) * lambda(0, 0) + x[0] * lambda(1, 0) + x[1] * lambda(2, 0);
+}
+
+dual2nd TargetFunc(const VectorXdual2nd &x)
+{
+    return x[0] * x[0] + 2 * x[1] * x[1] + 2 * x[0] + 8 * x[1];
 }
 
 namespace OPTIMIZE::IPM
@@ -37,8 +65,16 @@ namespace OPTIMIZE::IPM
     class InteriorPointMethod
     {
     public:
-        explicit InteriorPointMethod(const VectorXdual2nd &data) : mData(data)
+        explicit InteriorPointMethod(const std::vector<double> &data) : mData(data)
         {
+            this->initDataVector();
+            this->initHessian();
+            this->computeConstrainsts();
+            this->computeConstrainstsJacobian();
+            this->initSlack();
+            this->initLambdaMultipler();
+            this->computeKKT();
+            this->computeConstrainstsHessian();
         }
 
         virtual ~InteriorPointMethod()
@@ -54,7 +90,7 @@ namespace OPTIMIZE::IPM
 
             for (auto i = 0; i < mOuterIterNum; i++)
             {
-                if (mGrad.norm() <= mKtol)
+                if (mKKT1.norm() <= mKtol && mKKT2.norm() <= mKtol && mKKT3.norm() <= mKtol && mKKT4.norm() <= mKtol)
                 {
                     mSignal = 1;
                     mKtolConverged = true;
@@ -64,12 +100,14 @@ namespace OPTIMIZE::IPM
                 for (auto j = 0; j < mInnerIterNum; j++)
                 {
                     // judge kkt
-                    if (mGrad.norm() <= mKtol)
+                    if (mKKT1.norm() <= mKtol && mKKT2.norm() <= mKtol && mKKT3.norm() <= mKtol && mKKT4.norm() <= mKtol)
                     {
                         mSignal = 1;
                         mKtolConverged = true;
                         break;
                     }
+
+                    // compute gradient and hessian
 
                     computeBackTrackingLineSearch();
 
@@ -100,79 +138,254 @@ namespace OPTIMIZE::IPM
 
         void print()
         {
-            std::cout << "mGrad = \n"
-                      << mGrad << std::endl;
-            std::cout << "mHessian = \n"
-                      << mHessian << std::endl;
+            // std::cout << "mFuncGrad = \n"
+            //           << mFuncGrad << std::endl;
+            // std::cout << "mFuncHessian = \n"
+            //           << mFuncHessian << std::endl;
         }
 
     private:
         int mIterCount = 0;
         int mSignal = 0;
-        int mOuterIterNum = 10;
+        int mOuterIterNum = 1;
         int mInnerIterNum = 20;
 
         double mEta = 1e-4;
         double mKtol = 1e-4;
         double mFtol = 1e-8;
         double mTau = 0.995;
+        double mMu = 0.2;
         bool mKtolConverged = false;
         bool mFtolConverged = false;
 
         dual2nd mPhi0, mLastPhi0;
-        VectorXdual mGrad;
-        Eigen::MatrixXd mHessian;
-        VectorXdual2nd mData;
+        VectorXdual mFuncGrad;
+        MatrixXd mFuncHessian;
+        MatrixXd mGrad;
+
+        std::vector<double> mData;
+        ArrayXreal mRealData;
+        VectorXdual2nd mDual2ndData;
+
+        MatrixXd mCi;
+        MatrixXd mConstrainJacobian;
+
+        MatrixXd mConstraintsFuncHessian;
+        MatrixXd mHessian;
+
+        MatrixXd mLambda, mSlack;
+        MatrixXd mKKT1, mKKT2, mKKT3, mKKT4;
+
+        void initDataVector()
+        {
+            ArrayXreal real_data(mData.size());
+            VectorXdual2nd dual2nd_data(mData.size());
+
+            for (auto i = 0; i < mData.size(); i++)
+            {
+                real_data[i] = mData[i];
+                dual2nd_data[i] = mData[i];
+            }
+
+            mRealData = real_data;
+            mDual2ndData = dual2nd_data;
+        }
+
+        void initSlack()
+        {
+            MatrixXd slack(3, 1);
+            slack << 1e-4, 1e-4, 1e-4;
+
+            mSlack = slack;
+
+            // std::cout << "mSlack = \n"
+            //           << mSlack << std::endl;
+        }
+
+        void initLambdaMultipler()
+        {
+            MatrixXd lambda = mConstrainJacobian.completeOrthogonalDecomposition().pseudoInverse() * mFuncGrad.cast<double>();
+
+            mLambda = lambda;
+            for (auto i = 0; i < mLambda.rows(); i++)
+                for (auto j = 0; j < mLambda.cols(); j++)
+                    if (mLambda(i, j) < 0.0)
+                        mLambda(i, j) = 1e-4;
+
+            // std::cout << "init mLambda = \n"
+            //           << mLambda << std::endl;
+        }
+
+        void initHessian()
+        {
+            mFuncHessian = hessian(TargetFunc, wrt(mDual2ndData), at(mDual2ndData), mPhi0, mFuncGrad);
+
+            KIRI_LOG_DEBUG("mFuncHessian={0}", mFuncHessian);
+        }
+
+        void computeConstrainsts()
+        {
+            auto constrain0 = double(Constrainst0(mRealData));
+            VectorXd constrain1 = Constrainst1(mRealData).cast<double>();
+
+            MatrixXd constrain_i(constrain1.size() + 1, 1);
+            constrain_i << constrain0, constrain1;
+
+            mCi = constrain_i;
+
+            // std::cout << "mCi = \n"
+            //           << mCi << std::endl;
+        }
+
+        void computeConstrainstsJacobian()
+        {
+            real u;
+            VectorXd grad0 = gradient(Constrainst0, wrt(mRealData), at(mRealData), u);
+
+            VectorXreal F;
+            MatrixXd grad1 = jacobian(Constrainst1, wrt(mRealData), at(mRealData), F);
+
+            MatrixXd constrainst_jacobian(grad0.rows(), grad0.cols() + grad1.cols());
+            constrainst_jacobian << grad0, grad1;
+            mConstrainJacobian = constrainst_jacobian;
+        }
+
+        void computeConstrainstsHessian()
+        {
+
+            dual2nd u;
+            VectorXdual g;
+            mConstraintsFuncHessian = hessian(ConstrainstFunc, wrt(mDual2ndData), at(mDual2ndData, mLambda), u, g);
+
+            KIRI_LOG_DEBUG("mConstraintsFuncHessian={0}", mConstraintsFuncHessian);
+
+            // Hessian of the Lagrangian
+            MatrixXd d2L = mFuncHessian - mConstraintsFuncHessian;
+
+            KIRI_LOG_DEBUG("d2L={0}", d2L);
+
+            MatrixXd d2LUpper = d2L.triangularView<Eigen::Upper>();
+            KIRI_LOG_DEBUG("d2LUpper={0}", d2LUpper);
+
+            auto sigma = mSlack;
+            for (auto i = 0; i < sigma.rows(); i++)
+            {
+                for (auto j = 0; j < sigma.cols(); j++)
+                {
+                    sigma(i, j) = mLambda(i, j) / (mSlack(i, j) + MEpsilon<float>());
+                }
+            }
+
+            auto diagSize = sigma.array().size();
+            MatrixXd SigmaDiag(diagSize, diagSize);
+
+            SigmaDiag = sigma.array().matrix().asDiagonal();
+            KIRI_LOG_DEBUG("sigma={0}", SigmaDiag);
+
+            MatrixXd eyeConstrainMatrix = -MatrixXd::Identity(3, 3);
+
+            MatrixXd HessianColConcate = MatrixXd::Zero(mData.size(), mData.size() + 2 * 3);
+            MatrixXd VarConstrainZeroMatrix = MatrixXd::Zero(mData.size(), 3);
+            HessianColConcate << d2LUpper, VarConstrainZeroMatrix, mConstrainJacobian;
+
+            MatrixXd HessianColConcate1 = MatrixXd::Zero(3, mData.size() + 2 * 3);
+            MatrixXd ConstrainVarZeroMatrix = MatrixXd::Zero(3, mData.size());
+            HessianColConcate1 << ConstrainVarZeroMatrix, SigmaDiag, eyeConstrainMatrix;
+
+            MatrixXd HessianRowConcate = MatrixXd::Zero(mData.size() + 3, mData.size() + 2 * 3);
+            HessianRowConcate << HessianColConcate, HessianColConcate1;
+
+            MatrixXd ConstrainVarPlusTwoConstrainMatrix = MatrixXd::Zero(3, mData.size() + 2 * 3);
+
+            MatrixXd hessianMatrix(mData.size() + 2 * 3, mData.size() + 2 * 3);
+            hessianMatrix << HessianRowConcate, ConstrainVarPlusTwoConstrainMatrix;
+
+            MatrixXd hessianupperMatrix = hessianMatrix.triangularView<Eigen::Upper>();
+            hessianMatrix = hessianupperMatrix + hessianupperMatrix.transpose();
+
+            MatrixXd hessianHalfDiagMatrix = (hessianMatrix.diagonal() / 2).array().matrix().asDiagonal();
+            hessianMatrix -= hessianHalfDiagMatrix;
+
+            mHessian = hessianMatrix;
+            // KIRI_LOG_DEBUG("hessianMatrix=\n{0}", hessianMatrix);
+        }
 
         void computeKKT()
         {
-            mHessian = hessian(targetFunc, wrt(mData), at(mData), mPhi0, mGrad);
+
+            auto grad2 = mSlack;
+            auto kkt2 = mSlack;
+            for (auto i = 0; i < kkt2.rows(); i++)
+            {
+                for (auto j = 0; j < kkt2.cols(); j++)
+                {
+                    kkt2(i, j) = (mLambda(i, j) - mMu / (mSlack(i, j) + MEpsilon<float>())) * mSlack(i, j);
+                    grad2(i, j) = (mLambda(i, j) - mMu / (mSlack(i, j) + MEpsilon<float>()));
+                }
+            }
+
+            mKKT1 = mFuncGrad.cast<double>() - mConstrainJacobian * mLambda;
+
+            mKKT2 = kkt2;
+
+            mKKT3 = MatrixXd::Zero(1, 1);
+
+            mKKT4 = mCi - mSlack;
+
+            KIRI_LOG_DEBUG("KKT1=\n{0}", mKKT1);
+            KIRI_LOG_DEBUG("KKT2=\n{0}", mKKT2);
+            KIRI_LOG_DEBUG("KKT3=\n{0}", mKKT3);
+            KIRI_LOG_DEBUG("KKT4=\n{0}", mKKT4);
+
+            mGrad = MatrixXd(mKKT1.rows() + mKKT2.rows() + mKKT4.rows(), 1);
+            mGrad << mKKT1, grad2, mKKT4;
+            KIRI_LOG_DEBUG("mGrad=\n{0}", mGrad);
         }
 
         void computeBackTrackingLineSearch(double alphaSMax = 1.0, double alphaLMax = 1.0)
         {
-            auto correction = false;
-            auto alphaCorr = 1.0;
+            // auto correction = false;
+            // auto alphaCorr = 1.0;
 
-            mPhi0 = targetFunc(mData);
+            // mPhi0 = targetFunc(mData);
 
-            auto dir = -(mHessian.inverse() * mGrad);
-            std::cout << "search direction=" << dir.cast<dual2nd>() << std::endl;
+            // auto dir = -(mFuncHessian.inverse() * mFuncGrad);
+            // std::cout << "search direction=" << dir.cast<dual2nd>() << std::endl;
 
-            auto dphi0 = (mGrad.transpose() * dir).cast<dual2nd>()(0, 0);
+            // auto dphi0 = (mFuncGrad.transpose() * dir).cast<dual2nd>()(0, 0);
 
-            // equality constraints or unconstrained problems
-            auto phi1 = targetFunc(mData + alphaSMax * dir.cast<dual2nd>());
-            auto phi1_prime = mPhi0 + alphaSMax * mEta * dphi0;
-            // std::cout << "phi1=" << phi1 << "; phi1 prime=" << phi1_prime << std::endl;
-            if (phi1 > phi1_prime)
-            {
-                if (!correction)
-                {
-                    alphaSMax *= mTau;
-                    alphaLMax *= mTau;
+            // // equality constraints or unconstrained problems
+            // auto phi1 = targetFunc(mData + alphaSMax * dir.cast<dual2nd>());
+            // auto phi1_prime = mPhi0 + alphaSMax * mEta * dphi0;
+            // // std::cout << "phi1=" << phi1 << "; phi1 prime=" << phi1_prime << std::endl;
+            // if (phi1 > phi1_prime)
+            // {
+            //     if (!correction)
+            //     {
+            //         alphaSMax *= mTau;
+            //         alphaLMax *= mTau;
 
-                    while (targetFunc(mData + alphaSMax * dir.cast<dual2nd>()) > mPhi0 + alphaSMax * mEta * dphi0)
-                    {
-                        // backtracking line search
-                        if ((alphaSMax * dir.cast<dual2nd>()).norm() < std::numeric_limits<dual2nd>::epsilon())
-                        {
-                            // search direction is unreliable to machine precision, stop solver
-                            mSignal = -2;
-                            return;
-                        }
-                        alphaSMax *= mTau;
-                        alphaLMax *= mTau;
-                    }
-                }
-            }
+            //         while (targetFunc(mData + alphaSMax * dir.cast<dual2nd>()) > mPhi0 + alphaSMax * mEta * dphi0)
+            //         {
+            //             // backtracking line search
+            //             if ((alphaSMax * dir.cast<dual2nd>()).norm() < std::numeric_limits<dual2nd>::epsilon())
+            //             {
+            //                 // search direction is unreliable to machine precision, stop solver
+            //                 mSignal = -2;
+            //                 return;
+            //             }
+            //             alphaSMax *= mTau;
+            //             alphaLMax *= mTau;
+            //         }
+            //     }
+            // }
 
-            if (correction)
-                mData += alphaSMax * dir.cast<dual2nd>();
-            else
-                mData += alphaSMax * dir.cast<dual2nd>();
+            // if (correction)
+            //     mData += alphaSMax * dir.cast<dual2nd>();
+            // else
+            //     mData += alphaSMax * dir.cast<dual2nd>();
 
-            std::cout << "searched new=" << mData << std::endl;
+            // std::cout << "searched new=" << mData << std::endl;
         }
     };
 
--- demos/interior_point_method/src/main.cpp
+++ demos/interior_point_method/src/main.cpp
@@ -2,12 +2,20 @@
  * @Author: Xu.WANG raymondmgwx@gmail.com
  * @Date: 2022-06-15 16:46:01
  * @LastEditors: Xu.WANG raymondmgwx@gmail.com
- * @LastEditTime: 2022-06-17 11:06:07
+ * @LastEditTime: 2022-06-20 14:00:39
  * @FilePath: \Kiri2D\demos\interior_point_method\src\main.cpp
  * @Description:
  * @Copyright (c) 2022 by Xu.WANG raymondmgwx@gmail.com, All Rights Reserved.
  */
-
+/***
+ * @Author: Xu.WANG raymondmgwx@gmail.com
+ * @Date: 2022-06-15 16:46:01
+ * @LastEditors: Xu.WANG raymondmgwx@gmail.com
+ * @LastEditTime: 2022-06-20 00:26:54
+ * @FilePath: \Kiri2D\demos\interior_point_method\src\main.cpp
+ * @Description:
+ * @Copyright (c) 2022 by Xu.WANG raymondmgwx@gmail.com, All Rights Reserved.
+ */
 #include <kiri2d.h>
 using namespace KIRI2D;
 
@@ -20,17 +28,134 @@ std::string get_shape(const EigenBase<Derived> &x)
     oss << "(" << x.rows() << ", " << x.cols() << ")";
     return oss.str();
 }
+#include <Eigen/QR>
+#include <autodiff/forward/real.hpp>
+#include <autodiff/forward/real/eigen.hpp>
+// The scalar function for which the gradient is needed
+real f(const ArrayXreal &x)
+{
+    return x[0] + 2 * x[1] - 10;
+}
+
+VectorXreal f1(const VectorXreal &x)
+{
+    return x;
+}
+
+dual2nd original_func(const VectorXdual2nd &x)
+{
+    return x[0] * x[0] + 2 * x[1] * x[1] + 2 * x[0] + 8 * x[1];
+}
 
 int main(int argc, char *argv[])
 {
     // log system
     KiriLog::init();
 
-    VectorXdual2nd data(2);
-    data << -2.1433, 0.2949;
+    std::vector<double> data;
+    data.emplace_back(-0.6165);
+    data.emplace_back(-0.1307);
 
     auto ipm = std::make_shared<OPTIMIZE::IPM::InteriorPointMethod>(data);
-    ipm->solve();
+    // ipm->solve();
+
+    // using Eigen::MatrixXd;
+    // using Eigen::VectorXd;
+    // ArrayXreal x(2); // the input array x with 5 variables
+    // x << -0.6165, -0.1307;
+
+    // real u; // the output scalar u = f(x) evaluated together with gradient below
+
+    // VectorXd g = gradient(f, wrt(x), at(x), u); // evaluate the function value u and its gradient vector g = du/dx
+
+    // std::cout << "u = " << u << std::endl; // print the evaluated output u
+    // std::cout << "g = \n"
+    //           << g << std::endl; // print the evaluated gradient vector g = du/dx
+
+    // VectorXreal F;
+    // MatrixXd J = jacobian(f1, wrt(x), at(x), F);
+    // std::cout << "J = \n"
+    //           << J << std::endl;
+    // // ci
+    // auto f_general0 = double(f(x));
+    // VectorXd f_general1 = f1(x).cast<double>();
+    // MatrixXd Ci(f_general1.size() + 1, 1);
+    // Ci << f_general0, f_general1;
+    // std::cout << "Ci = \n"
+    //           << Ci << std::endl;
+
+    // // dci
+    // MatrixXd C(g.rows(), g.cols() + J.cols());
+    // C << g, J;
+
+    // std::cout << "C = \n"
+    //           << C << std::endl;
+
+    // MatrixXd D = -MatrixXd::Identity(x.size() + 1, x.size() + 1);
+    // std::cout << "D = \n"
+    //           << D << std::endl;
+
+    // MatrixXd E(C.rows() + D.rows(), C.cols());
+    // E << C, D;
+    // std::cout << "E = \n"
+    //           << E << std::endl;
+
+    // dual2nd mPhi0;
+    // VectorXdual mGrad;
+    // VectorXdual2nd data(2);
+    // data << -0.6165, -0.1307;
+
+    // Eigen::MatrixXd mHessian = hessian(original_func, wrt(data), at(data), mPhi0, mGrad);
+
+    // std::cout << "mGrad = \n"
+    //           << mGrad.cast<double>() << std::endl;
+
+    // auto gradXD = mGrad.cast<double>();
+    // MatrixXd lambda = C.completeOrthogonalDecomposition().pseudoInverse() * mGrad.cast<double>();
+
+    // std::cout << "lambda = \n"
+    //           << lambda << std::endl;
+
+    // for (auto i = 0; i < lambda.rows(); i++)
+    // {
+    //     for (auto j = 0; j < lambda.cols(); j++)
+    //     {
+    //         if (lambda(i, j) < 0.0)
+    //             lambda(i, j) = 1e-4;
+    //     }
+    // }
+
+    // std::cout << "lambda = \n"
+    //           << lambda << std::endl;
+
+    // MatrixXd grad_tmp(data.size() + 2 * 3, 1);
+    // // C << g, J;
+    // auto grad_1 = mGrad.cast<double>() - C * lambda; //  // - C.dot(lambda);
+
+    // MatrixXd slack(3, 1);
+    // slack << 1e-4, 1e-4, 1e-4;
+
+    // auto tmp_slack = slack;
+    // for (auto i = 0; i < tmp_slack.rows(); i++)
+    // {
+    //     for (auto j = 0; j < tmp_slack.cols(); j++)
+    //     {
+    //         tmp_slack(i, j) = 0.2 / (tmp_slack(i, j) + MEpsilon<float>());
+    //     }
+    // }
+
+    // std::cout << "tmp_slack = \n"
+    //           << tmp_slack << std::endl;
+
+    // auto grad_2 = lambda - tmp_slack;
+
+    // std::cout << "grad_2 = \n"
+    //           << grad_2 << std::endl;
+
+    // auto grad_3 = Ci - slack;
+
+    // std::cout << "grad_3 = \n"
+    //           << grad_3 << std::endl;
 
     // dual2nd dual_u;
     // VectorXdual dual_g;
